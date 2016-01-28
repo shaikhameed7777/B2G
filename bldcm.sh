605bc6a4d3cbdb25664cc3db52e19408ad016776
Adam Farden <adam@farden.cz>
2016-01-28 22:31:22 +0100

    Allow bldcm.sh to pass an option

--- bldcm.sh
+++ bldcm.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+build_option=gecko-update-fota-fullimg
+
+if [ -n "$1" ]; then
+    build_option=$1
+fi;
+
 export VARIANT=userdebug &&
 export B2G_UPDATER=1 &&
 export B2G_SYSTEM_APPS=1 &&
@@ -13,4 +19,4 @@ export LOCALES_FILE=$PWD/gaia/locales/languages_all.json &&
 export GAIA_DEFAULT_LOCALE=en-GB &&
 export ENABLE_DEFAULT_BOOTANIMATION=true &&
 export DOGFOOD=1 &&
-./build.sh gecko-update-fota-fullimg
+./build.sh $build_option
